fcdd95622bdc6101098cccb9958d0f49b2149408
frankcouchman-bit <frankcouchman@gmail.com>
2025-10-16 04:51:51 +0200

    Add Footer component with copyright notice

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+// components/Footer.tsx
+const Footer: React.FC = () => (
+  <footer className="bg-gray-100 text-center py-4">
+    <p className="text-sm text-gray-600">&copy; 2025 SEOScribe. All rights reserved.</p>
+  </footer>
+);
+export default Footer;
