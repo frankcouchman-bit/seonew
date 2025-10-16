99c9dc2e0154d4790af6f598fb7365c24aca32d3
frankcouchman-bit <frankcouchman@gmail.com>
2025-10-16 04:50:38 +0200

    Add custom _document.tsx for Next.js
    Create a custom Document for Next.js with favicon and meta tags.

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+// pages/_document.tsx
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* Favicon */}
+          <link rel="icon" href="/favicon.ico" />
+          {/* Default meta tags */}
+          <meta charSet="UTF-8" />
+          <meta name="theme-color" content="#ffffff" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
